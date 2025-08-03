1c21030034f08bc93e8ef62f6b92e1ee5e295044
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-08-03 22:46:32 +0300

    react-hooks-lists-and-keys-lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,8 +6,12 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+        {technologies.map((tech, index) => (
+          <span key={index} className="technology">
+            {tech}
+          </span>
+        ))}
+        </div>
     </div>
   );
 }
